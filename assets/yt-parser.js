34eaf8de169554b69dadacb7a9424469fabe1e4a
Pablo H. Paladino <paladinop@gmail.com>
2015-01-22 14:52:10 -0300

    YT Api Explorer is dead, long live YT Api Explorer

--- assets/yt-parser.js
+++ assets/yt-parser.js
@@ -8,12 +8,18 @@ var YT2CSV;
 
     YT2CSV.entries = [];
 
+    YT2CSV.endpoint = "http://gdata.youtube.com/feeds/api/videos?alt=json";
+
+    YT2CSV.extraParams = '&v=2&fields=@gd:fields,entry(@gd:fields,published,updated, category(@label), title, content, link(@href),author, gd:comments/gd:feedLink(@countHint), media:group/media:content(@duration), media:group/media:thumbnail, gd:rating(@min), gd:rating(@max), gd:rating(@average), gd:rating(@numRaters), yt:statistics(@favoriteCount), yt:statistics(@viewCount))&callback=?';
+
     YT2CSV.init = function () {
     	$('#send').on('click',function(){
             _gaq.push(['_trackEvent', 'button', 'dale']);
             $('#loader').show();
             $('.csv,.json').hide();
-            $.getJSON($('#url').val()+'&fields=@gd:fields,entry(@gd:fields,published,updated, category(@label), title, content, link(@href),author, gd:comments/gd:feedLink(@countHint), media:group/media:content(@duration), media:group/media:thumbnail, gd:rating(@min), gd:rating(@max), gd:rating(@average), gd:rating(@numRaters), yt:statistics(@favoriteCount), yt:statistics(@viewCount))&callback=?',
+            var query = YT2CSV.endpoint+'&'+$("#filters input[value!=''], #filters select:has(option[value!='']:selected)").serialize()+YT2CSV.extraParams;
+
+            $.getJSON(query,
                 function(data){
 	    			if(data && data.feed && data.feed.entry) {
 		    			YT2CSV.entries = data.feed.entry;
